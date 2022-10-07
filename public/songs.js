ef783701d79db99f09d21c1c7e23486041bea6fa
Glitch (harry-styles-heardle) <none>
2022-10-07 04:10:49 +0000

    🐙👞 Checkpoint ./public/songs.js:49396120/399

--- public/songs.js
+++ public/songs.js
@@ -41,8 +41,8 @@ let songs = [
     answer: "SAINt JHN - Roses (Imanbek Remix)",
   }, // October 5
   {
-    url: "https://soundcloud.com/saintjhn/roses-imanbek-remix?si=e11887bf9c3a48b49c047550f869d0a6&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
-    answer: "SAINt JHN - Roses (Imanbek Remix)",
+    url: "https://soundcloud.com/mikewilliamsdj/face-up-to-the-sun-feat-sara?si=8bc4565e37cb4e9c9d1798b555f8896b&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+    answer: "Mike Williams, Justin Mylo - Face Up To The Sun",
   },
   {
     url: "https://soundcloud.com/mikewilliamsdj/face-up-to-the-sun-feat-sara?si=8bc4565e37cb4e9c9d1798b555f8896b&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
